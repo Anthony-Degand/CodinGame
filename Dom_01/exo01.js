40148f216919adc0a5ba9b4961958369af0381ec
Anthony <Degand.anthony@gmail.com>
2022-07-12 13:41:20 +0200

    12/07/22 13:41

--- Dom_01/exo01.js
+++ Dom_01/exo01.js
@@ -1,8 +1,28 @@
 //1
 document.querySelector("h1").innerHTML = "Rick Astley - Never Gonna Give You Up";
-
+//rempalcer le html de l'element h1 par se texte 
 //2
+
+//3
 
 //4
 document.getElementById("erreur").remove();
+//supprimer l'element avec l'id erreur
+
+//5
+
+const para = document.createElement("footer");
+
+const node = document.createTextNode("© Copyright 2020 - Nom");
+
+para.appendChild(node);
+
+const element = document.getElementById("erreur");
+
+const child = document.querySelector("erreur p");
+
+element.insertBefore (para,body);
+
+//faux
+
 

438d90cb196e7f7e3ee45300a2ba8179f102192c
Anthony <Degand.anthony@gmail.com>
2022-07-13 15:33:35 +0200

    13/07/22 15:33

--- Dom_03/exo03.js
+++ Dom_03/exo03.js
@@ -1,30 +1,36 @@
 //1  masquer les paroles
-$(document).ready(function(){
-    $("#masquer-paroles").click(function(){
-        $("#paroles").toggle();
-        $("#masquer-paroles").html("Afficher les paroles");
-    });
+$(document).ready(function () {
+  $("#masquer-paroles").click(function () {
+    $("#paroles").toggle();
   });
+});
 
 //2
-$(document).ready(function(){
-  $("#masquer-refrains").click(function(){
-      $(".refrain").toggle();
+$(document).ready(function () {
+  $("#masquer-refrains").click(function () {
+    $(".refrain .contenu").toggle();
+
+
+    if ($("#masquer-refrains").is(":checked")) {
+      $(".hidden").css("display", "inline");
+    }
+
+    else {
+      $(".hidden").css("display", "none");
+    }
   });
 });
 
-/*$("label").append('<label><input type="checkbox" name="refrain" id="afficher-refrain"/> Afficher les refrains </label>');
 
-$(document).ready(function(){
-  $("#masquer-refrains").checked(function(){
-    $("#masquer-refrains").hide();
-      $("#masquer-refrains").replaceWith("#Afficher-refrains");
 
+//3
+
+$(document).ready(function () {
+  $(".definition").hover(function () {
+
+    $(".contenu").css("display", "inline");
+  }, function () {
+    $(".contenu").css("display", "none");
   });
 });
 
-$(document).ready(function(){
-  $("#afficher-refrains").checked(function(){
-      $("#masquer-refrains").replaceWith("#afficher-refrain");
-  });
-});*/
